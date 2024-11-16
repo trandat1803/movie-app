0bdbe807efeafec4fc825a12243e1d694650e420
trandat1803 <quocdat@MacprocuaDatne.local>
2024-11-16 14:47:32 +0700

    add form base

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,26 +1,28 @@
-  import axios from "axios";  
-  import { BASE_URL } from "./config";
+import axios from "axios";
+import { BASE_URL } from "./config";
 
-  const apiService = axios.create({
-    baseURL: BASE_URL,
-  })
+const apiService = axios.create({
+  baseURL: BASE_URL,
+});
 
-  apiService.interceptors.request.use(
-    (request) => {
-      console.log("Start the request", request);
-      return request;
-    },function(error){
-      console.log("Request Error:" + error)
-    }
-  );
+apiService.interceptors.request.use(
+  (request) => {
+    console.log("Start the request", request);
+    return request;
+  },
+  function (error) {
+    console.log("Request Error:" + error);
+  }
+);
 
-  apiService.interceptors.request.use(
-    (request) => {
-      console.log("Response", request);
-      return request;
-    },function(error){
-      console.log("Request Error:" + error)
-    }
-  );
+apiService.interceptors.request.use(
+  (request) => {
+    console.log("Response", request);
+    return request;
+  },
+  function (error) {
+    console.log("Request Error:" + error);
+  }
+);
 
 export default apiService;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { fetchMovies } from '../api';
-import apiService  from '../api';
+import apiService from "../app/apiService";
+import {set, useForm} from "react-hook-form";
+import { Container } from '@mui/material';
+//import { API_KEY } from '../app/config';
 
-const HomePage = () => {
+function HomePage() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  
 
   useEffect(() => {
     const loadMovies = async () => {
-      const res = await apiService.get(`genre/movie/list?api_key=148c8c91a329d210cfc8b8b2ed6c79d0&language=en-US`)
+      setLoading(true);
+      try {
+        const res = await apiService.get(`genre/movie/list?api_key=148c8c91a329d210cfc8b8b2ed6c79d0&language=en`);
+        setMovies(res.data);
+        setError("");
+      }catch(error){
+        console.log(error);
+        setError(error.message);
+      }
+      setLoading(false);
       // const moviesData = await fetchMovies();
       // setMovies(moviesData);
-      console.log(res);
+      //console.log(res);
     };
     loadMovies();
   }, []);
 
   return (
-    <div className="homepage">
-      <h1>Popular Movies</h1>
-      <div className="movie-grid">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-            <h2>{movie.title}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container xs={{display: "flex", minHeight: "100vh", mt: 3}}>
+      <Stack>
+        
+      </Stack>
+    </Container>
   );
 };
 
